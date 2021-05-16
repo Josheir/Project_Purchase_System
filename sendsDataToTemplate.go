be7112d9530c763901605ff6819ba33eebd59e0a
Joshua Eirman <joshuaeirm@gmail.com>
2021-05-16 19:11:33 -0400

    forgot, worked on last page

---
+++ sendsDataToTemplate.go
@@ -0,0 +1,38 @@
+
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nId := r.URL.Query().Get("id")
+	selDB, err := db.Query("SELECT * FROM Employee WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Employee{}
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+	}
+	tmpl.ExecuteTemplate(w, "Show", emp)
+	defer db.Close()
+}
+
+func purgeHTML(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	//has an id value passed in url
+	mux.HandleFunc("/purgeHTML", template2)
+
+	http.ListenAndServe(":8080", mux)
+}
