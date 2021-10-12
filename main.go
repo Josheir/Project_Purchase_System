55a83997a07c283b98db0e2b7cc2a5cf1a6bc831
Joshua Eirman <joshuaeirm@gmail.com>
2021-10-12 15:54:04 -0400

    data is passing not an array yet

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+
+	//"errors"
 	"fmt"
 	"html/template"
+	//"io/ioutil"
+
+	//"log"
+	//"strings"
 
 	//"bytes"
 	"net/http"
@@ -1351,17 +1357,275 @@ type try1 struct {
 }
 
 //type geoData[4]
-type display_ struct {
+
+type display5 struct {
 	Var   string `json:"var"`
-	Id    int    `json:"id"`
-	Quant int    `json:"quant"`
-	Uid   int    `json:"uid"`
+	Id    string `json:"id"`
+	Quant string `json:"quant"`
+	Uid   string `json:"uid"`
+}
+
+type Display3 struct {
+	Var int `json:"var"`
 }
 
 func display1(w http.ResponseWriter, r *http.Request) {
 
-	/////////////
+	//w.Header().Set("Content-Type", "application/json")
+
+	var myArray display5
+
+	c := json.NewDecoder(r.Body)
+	var err80 = c.Decode(&myArray)
+	if err80 != nil {
+		fmt.Println(err80)
+		fmt.Println(c)
+		fmt.Println(myArray)
+
+	}
+
+	//err78 := json.Unmarshal([]byte(c), &myArray)
+	//if err78 != nil {
+	//	fmt.Println(c)
+//
+//	}
+
+	/*
+	//req := endpoint.AddRequest{}
+
+	//req := endpoint.AddRequest{}
+
+	//req, err := http.NewRequest("POST", url, strings.NewReader(form.Encode()))
+
+	//var unknown map[string]interface{}
+
+	var myArray []display5
+
+	b, err77 := ioutil.ReadAll(r.Body)
+	var c = string(b)
+	fmt.Println(c)
+
+	//r.ParseForm()
+	//err79 := json.NewDecoder(r.Body).Decode(&myArray)
+	//if err79 != nil {
+	//	fmt.Print(err79)
+	//	fmt.Println(c)
+	//
+	//}
+
+	err78 := json.Unmarshal([]byte(c), &myArray)
+	if err78 != nil {
+		fmt.Println(c)
+
+	}
+
+	fmt.Println(c)
+	fmt.Println(myArray)
+
+	for index, element := range myArray {
+		fmt.Println(index, "=>", element)
+	}
+
+	//unknown[]
+	//	fmt.Println(unknown[0])
+
+	fmt.Println(b)
+	fmt.Println(err77)
+
+	c = string(b)
+	fmt.Println(string(b))
+	fmt.Println(c)
+*/
+	r.ParseForm()
+	var body1 string
+	for key, _ := range r.Form {
+		body1 = string(key)
+		fmt.Println(body1)
+		fmt.Println(string(body1))
+		break
+	}
+
+	array := r.Form["var"]
+	fmt.Println(array)
+
+	var a = r.FormValue("var")
+	fmt.Println(a)
 	queries := r.URL.Query()
+	fmt.Println(queries)
+
+	//var display2 Display2
+	//r.ParseForm()
+	//err3 := json.NewDecoder(r.Body).Decode(&display2)
+	//if err3 != nil {
+	//	fmt.Println(err3)
+	//	fmt.Println(r.Body)
+	//	//fmt.Println(r.)
+	//
+	//	}
+
+	//val12, err100 := r.GetBody()
+
+	//fmt.Println(val12)
+	//fmt.Println(err100)
+
+	////////1591
+	/*r.ParseForm()
+	userid := r.Form["Var"]
+	fmt.Println(userid)
+
+	fmt.Println(r.Body)
+	//var p display__
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	//err9 := dec.Decode(&p)
+	if err9 != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+
+		switch {
+		// Catch any syntax errors in the JSON and send an error message
+		// which interpolates the location of the problem to make it
+		// easier for the client to fix.
+		case errors.As(err9, &syntaxError):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// In some circumstances Decode() may also return an
+		// io.ErrUnexpectedEOF error for syntax errors in the JSON. There
+		// is an open issue regarding this at
+		// https://github.com/golang/go/issues/25956.
+		case errors.Is(err9, io.ErrUnexpectedEOF):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON")
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// Catch any type errors, like trying to assign a string in the
+		// JSON request body to a int field in our Person struct. We can
+		// interpolate the relevant field name and position into the error
+		// message to make it easier for the client to fix.
+		case errors.As(err9, &unmarshalTypeError):
+			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// Catch the error caused by extra unexpected fields in the request
+		// body. We extract the field name from the error message and
+		// interpolate it in our custom error message. There is an open
+		// issue at https://github.com/golang/go/issues/29035 regarding
+		// turning this into a sentinel error.
+		case strings.HasPrefix(err9.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err9.Error(), "json: unknown field ")
+			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// An io.EOF error is returned by Decode() if the request body is
+		// empty.
+		case errors.Is(err9, io.EOF):
+			msg := "Request body must not be empty"
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// Catch the error caused by the request body being too large. Again
+		// there is an open issue regarding turning this into a sentinel
+		// error at https://github.com/golang/go/issues/30715.
+		case err9.Error() == "http: request body too large":
+			msg := "Request body must not be larger than 1MB"
+			http.Error(w, msg, http.StatusRequestEntityTooLarge)
+
+		// Otherwise default to logging the error and sending a 500 Internal
+		// Server Error response.
+		default:
+			log.Println(err9.Error())
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	//err3 := json.NewDecoder(r.Body).Decode(&p)
+	//if err3 != nil {
+	//	fmt.Println(err3)
+	//	fmt.Println(r.Body)
+	//	//fmt.Println(r.)
+	//
+	//}
+
+	//err5 := r.ParseForm()
+	//if err5 != nil {
+	//	fmt.Println(err5)
+	//}
+
+	//userid := r.Form["Id"][0]
+	//fmt.Println(userid)
+
+	//val3 := r.FormValue("mydata")
+	//fmt.Println(val3)
+
+	var result interface{}
+	err7 := json.NewDecoder(r.Body).Decode(&result)
+	if err7 != nil {
+
+	}
+	//err5 := r.ParseForm()
+	//if err5 != nil {
+	//	fmt.Println(err5)
+	//}
+	//userid := r.Form["var"][0]
+	//fmt.Println(userid)
+
+	var j = `{"var":"test","id":10,"quant":1,"uid":1}`
+
+	var it display_
+
+	val11 := json.Unmarshal([]byte(j), &it)
+	fmt.Println(val11)
+
+	if err7 != nil {
+		panic(err7)
+	}
+	//fmt.Println(it)
+
+	//	body, err6 := ioutil.ReadAll(r.Body)
+	//if err6 != nil {
+	//    panic(err6)
+	//}
+	//
+	//fmt.Println(body)
+
+	//fmt.Println(key)
+
+	//	var p display_
+
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	//  err3 := json.NewDecoder(r.Body).Decode(&p)
+	//	if err3 != nil {
+	//		fmt.Println(w, err3)
+	//	}
+
+	//fmt.Println(p)
+
+	err4 := r.ParseForm()
+	if err4 != nil {
+		fmt.Println(w, err4)
+	}
+
+	//query := r.URL.Query()
+
+	//r.Body
+	//userid, present := query["userid"]
+	//userid := r.Form["Var"][0]
+	//userid = r.Form["Id"][1]
+	//userid = r.Form["Id"][2]
+
+	//for key, val := range userid {
+	///
+	//		fmt.Println(key)
+	//		fmt.Println(val)
+	//	}
+	//
+	//	fmt.Println(userid)
+
+	/////////////
+	//queries := r.URL.Query()
 	fmt.Println(queries)
 	val2 := ""
 	for key, val := range queries {
@@ -1372,15 +1636,6 @@ func display1(w http.ResponseWriter, r *http.Request) {
 		val2 = val[0]
 		fmt.Println(val2)
 
-	}
-
-	bytes := []byte(val2)
-	var display2 []display_
-	json.Unmarshal(bytes, &display2)
-
-	for l := range display2 {
-		fmt.Printf("Id = %v, Name = %v", display2[l].Id, display2[l].Var)
-		fmt.Println()
 	}
 
 	//var biArray [][]string
@@ -1405,7 +1660,7 @@ func display1(w http.ResponseWriter, r *http.Request) {
 
 	GlobCounter++
 	//store, err := session.Start(context.Background(), w, r)
-
+	*/
 	/*
 		err = r.ParseForm()
 
